8d6842b8d1f6b7edff0f760b21913ca13fecfe4e
evastakava <125592045+evastakava@users.noreply.github.com>
2023-02-18 16:37:27 +0000

    test

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def find_mismatch(text):
         elif next in ")]}":
             if not opening_brackets_stack:
                 return i + 1
-            last_opening_bracket = opening_brackets_stack.pop()
-            if not are_matching(last_opening_bracket.char, next):
+            top = opening_brackets_stack.pop()
+            if not are_matching(top.char, next):
                 return i + 1
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
